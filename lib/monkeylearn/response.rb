46089ca7dbb23dfbab9f07e6d63ca80558780169
Jonas Schulze <jonas.schulze@st.ovgu.de>
2018-06-21 19:49:47 +0200

    Refactor Response and MultiResponse
    Response: Add an instance variable that controls whether the response's
    keys should be symbolized. This will be used in the upcoming api_version
    key, that enables a backwards compatible format.
    
    MultiResponse: Having an attr_reader for `body` and a manually defined
    method with the same name, that is actually no reader for `@body`, is a
    bit confusing. Therefore, I renamed the method and made use of the
    attr_reader more "explicit".

--- lib/monkeylearn/response.rb
+++ lib/monkeylearn/response.rb
@@ -4,13 +4,14 @@ module Monkeylearn
 
     def initialize(raw_response)
       self.raw_response = raw_response
+      @symbolize_keys = true
     end
 
     def raw_response=(raw_response)
       @raw_response = raw_response
       @status = raw_response.status
       if raw_response.body != ''
-        @body = JSON.parse(raw_response.body, symbolize_keys: true)
+        @body = JSON.parse(raw_response.body, symbolize_keys: @symbolize_keys)
       else
         @body = nil
       end
@@ -27,17 +28,20 @@ module Monkeylearn
       self.responses = responses
     end
 
-    def body
-      responses.collect do |r|
-        r.body
-      end.reduce(:+)
-    end
-
     def responses=(responses)
       @responses = responses
+      @body = collect_body(responses)
       @plan_queries_allowed = @responses[-1].plan_queries_allowed
       @plan_queries_remaining = @responses[-1].plan_queries_remaining
       @request_queries_used = @responses.inject(0){|sum, r| sum + r.request_queries_used }
+    end
+
+    private
+
+    def collect_body(responses)
+      responses.collect do |r|
+        r.body
+      end.reduce(:+)
     end
   end
 end
